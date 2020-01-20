cce882a42378bcf567efee5c2f1199beeb4d9dcc
naseem <izzatnaseem30@gmail.com>
2020-01-21 00:42:54 +0200

    fixed

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# class-01
----
-## validator.js :
-I made doc to explain the function under it , this go to  validator.test.js  where the input as the  type I put and return boolean. 
+# LAB - Class 01          
 
-## validator.test.js:
-In the validator.test.js I make map to go throw types of things and made the less repetitive, and require change in the it Sentences.
- 
+## Project  Node Ecosystem
 
+### Author: Naseem
+
+### Links and Resources
+
+- [submission PR](https://github.com/naseem-401-advanced-javascript/class-01/tree/lab1)
+- [ci/cd](http://xyz.com) (GitHub Actions)
+
+
+### Setup
+
+#### `.env` requirements (where applicable)
+
+i.e.
+
+- `PORT` - Port Number
+- `MONGODB_URI` - URL to the running mongo instance/db
+
+#### How to initialize/run your application (where applicable)
+
+- e.g. `npm start`
+
+#### Tests
+
+- How do you run tests?
+- Any tests of note?
+- Describe any tests that you did not complete, skipped, etc
+
+#### UML
+
+Link to an image of the UML for your application and response to events
--- starter_code/validator.test.js
+++ starter_code/validator.test.js
@@ -12,13 +12,14 @@ describe('validator module performs basic validation of', () => {
     let obj = { x: 'y' };
     let func = () => { };
     let bool = false;
+    // global variable 
+    const dataTypes= [num,arr.obj,fun,bool]
     it('strings', () => {
-        const thingsType = [num, arr, obj, fun, bool]
         expect(validator.isString(str)).toBeTruthy();
-        thingsType.map(ele => {
+        dataType.map(ele => {
             expect(validator.isString(ele)).toBeFalsy();
 
-        })
+        });
 
         // expect(validator.isString(str)).toBeTruthy();
         // expect(validator.isString(num)).toBeFalsy();
@@ -51,25 +52,38 @@ describe('validator module performs basic validation of', () => {
 });
 
 describe('validator module performs complex validations', () => {
+    const naseem = {
+        id:'1993',
+        mame:'naseem izzat',
+        age:'26',
+        musicType:['classic','pure music'],
+        single:['yes']
+    };
 
     it('validates the presence of required object properties at any level', () => {
         // i.e. does person.hair.color exist and have a good value, not just person.hair
-        expect(true).toBeFalsy();
+        expect(naseem.name).toBeNull();
+        expect(nassem.gae).toBeEqual(24);
+
     });
 
     it('validates the proper types of object properties', () => {
         // i.e. person.name must be a string, etc.
-        expect(true).toBeFalsy();
+        expect(typeof naseem.name).toBeMatch('string');
     });
 
     it('validates the types of values contained in an array', () => {
         // i.e. an array of all strings or numbers
-        expect(true).toBeFalsy();
+        // expect(true).toBeFalsy();
+        naseem.musicType.forEach(element =>{
+            expect(typeof element).toBeMatch('string')
+        });
     });
 
     it('validates a value array against an approved list', () => {
         // i.e. a string might only be allowed to be "yes" or "no"
-        expect(true).toBeFalsy();
+        let example = naseem.single.includes('yes')
+        expect(true).toBeTruthy;
     });
 
     // TODO: Cover so, so many more cases
