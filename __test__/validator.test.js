de51f97ef6061ee2ad72e3e101d3e156a80ab31e
naseem <izzatnaseem30@gmail.com>
2020-01-25 19:36:34 +0200

    compleat

--- __test__/validator.test.js
+++ __test__/validator.test.js
@@ -13,11 +13,11 @@ describe('validator module performs basic validation of', () => {
     let func = () => { };
     let bool = false;
     // global variable 
-    const dataTypes= [num,arr.obj,fun,bool]
+    const dataTypes = [num, arr.obj, func, bool]
     it('strings', () => {
         expect(validator.isString(str)).toBeTruthy();
         dataTypes.forEach(ele => {
-            if (ele !== str){
+            if (ele !== str) {
                 expect(validator.isString(ele)).toBeFalsy();
             }
 
@@ -31,19 +31,21 @@ describe('validator module performs basic validation of', () => {
         // expect(validator.isString(bool)).toBeFalsy();
     });
 
+    //////expect(result of return from the validotor.js).
+
     it('numbers', () => {
         expect(validator.isNum(num)).toBeTruthy();
         dataTypes.forEach(element => {
             if (element != num) {
                 expect(validator.isNum(element)).toBeFalsy();
-            }  
-          });
-        })
+            }
+        });
+    })
 
     it('arrays', () => {
         expect(validator.isArr(arr)).toBeTruthy();
-        for ( let i= 0; i <dataTypes.length;i++){
-            if (dataTypes[i]==arr){
+        for (let i = 0; i < dataTypes.length; i++) {
+            if (dataTypes[i] == arr) {
                 continue;
             }
             expect(validator.isArr(dataTypes[i])).toBeFalsy();
@@ -52,8 +54,8 @@ describe('validator module performs basic validation of', () => {
 
     it('objects', () => {
         expect(validator.isObj(obj)).toBeTruthy();
-        dataTypes.forEach(element=>{
-            if (element!==obj){
+        dataTypes.forEach(element => {
+            if (element !== obj) {
                 expect(validator.isObj(element)).toBeFalsy();
             }
         })
@@ -61,8 +63,8 @@ describe('validator module performs basic validation of', () => {
 
     it('booleans', () => {
         expect(validator.isBoolean(bool)).toBeTruthy();
-        dataTypes.forEach(element=>{
-            if (element!==bool){
+        dataTypes.forEach(element => {
+            if (element !== bool) {
                 expect(validator.isBoolean(element)).toBeFalsy();
             }
         })
@@ -70,9 +72,9 @@ describe('validator module performs basic validation of', () => {
 
     it('functions', () => {
         expect(validator.isFunc(func)).toBeTruthy();
-        dataTypes.forEach(element=>{
-            if(element!==func){
-                expect(validator.isFunc(element)).toBeFalsy
+        dataTypes.forEach(element => {
+            if (element !== func) {
+                expect(validator.isFunc(element)).toBeFalsy();
             }
         })
     });
@@ -81,40 +83,41 @@ describe('validator module performs basic validation of', () => {
 
 describe('validator module performs complex validations', () => {
     const naseem = {
-        id:'1993',
-        name:'naseem izzat',
-        age:'26',
-        hopies:[],
-        musicType:['classic','pure music'],
-        anyThing:null,
-        single:['yes']
+        id: '1993',
+        name: 'naseem izzat',
+        age: '26',
+        hopies: [],
+        musicType: ['classic', 'pure music'],
+        anyThing: null,
+        single: ['yes']
     };
 
     it('validates the presence of required object properties at any level', () => {
         // i.e. does person.hair.color exist and have a good value, not just person.hair
         expect(naseem.anyThing).toBeNull();
-        expect(nassem.gae).toBeEqual(24);
+        expect(naseem.age).toEqual('26');
 
     });
 
     it('validates the proper types of object properties', () => {
         // i.e. person.name must be a string, etc.
-        expect(typeof naseem.name).toBeMatch('string');
+        expect(typeof naseem.name).toMatch('string');
     });
 
     it('validates the types of values contained in an array', () => {
         // i.e. an array of all strings or numbers
         // expect(true).toBeFalsy();
-        naseem.musicType.forEach(element =>{
-            expect(typeof element).toBeMatch('string')
+        naseem.musicType.forEach(element => {
+            expect(typeof element).toMatch('string')
         });
     });
-    
+
 
     it('validates a value array against an approved list', () => {
         // i.e. a string might only be allowed to be "yes" or "no"
         let example = naseem.single.includes('yes')
         expect(example).toBeTruthy;
+        // without is somthing beacuse i went  to test 
     });
 
     // TODO: Cover so, so many more cases
